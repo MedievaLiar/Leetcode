4666014106f08649db675bbfd037169253922e01
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2023-09-01 02:38:22 +0300

    Update 206.py

--- tasks/206.py
+++ tasks/206.py
@@ -1,18 +1,20 @@
-package main
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-func reverseList(head *ListNode) *ListNode {
-	current := head
-	var prev, next *ListNode
-	for current != nil {
-		next = current.Next
-		current.Next = prev
-		prev = current
-		current = next
-	}
-	return prev
-}
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from collections import deque
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        a = deque()
+        
+        pointer = head
+        while pointer:
+            a.append(pointer.val)
+            pointer = pointer.next
+        
+        head1 = None
+        while a:
+            head1 = ListNode(a.popleft(), head1)
+        
+        return head1
