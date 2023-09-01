c4df25de4bcaf07c136b05a2ea6ef2dc4ac7289a
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2023-09-01 03:55:43 +0300

    Update 206.py

--- tasks/206.py
+++ tasks/206.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+# faster than 70% 
 from collections import deque
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
