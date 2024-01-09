69fc3d010dfaa6dc920087f137a711a2a1635458
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-01-09 06:50:03 +0300

    Update 20.py

--- tasks/20.py
+++ tasks/20.py
@@ -1,62 +1,28 @@
-package main
+#beats 95%
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for gap in s:
+            if gap == '(' or gap == '{' or gap == '[':
+                stack.append(gap)
+            else:
+                if not stack:
+                    return False
+                match gap:
+                    case ')':
+                        if stack[-1] == '(':
+                            stack.pop()
+                        else:
+                            return False
+                    case '}':
+                        if stack[-1] == '{':
+                            stack.pop()
+                        else:
+                            return False                        
+                    case ']':
+                        if stack[-1] == '[':
+                            stack.pop()
+                        else:
+                            return False #эххх можно было бы и функцию сделать, жаль это всего лишь задача на литкоде
 
-import "fmt"
-
-func main() {
-	// 40 '(' 41 ')'
-	// 91 '[' 93 ']'
-	// 123 '{' 125 '}'
-	fmt.Print(isValid("([)]"))
-}
-
-func isValid(s string) bool {
-	// openedRound := 0
-	// openedSquare := 0
-	// openedFigure := 0
-	var opened []byte
-	if s[0] != ')' && s[0] != ']' && s[0] != '}' {
-		opened = append(opened, s[0])
-	} else {
-		return false
-	}
-
-	for i := 1; i < len(s); i++ {
-		switch s[i] {
-		case ')':
-			if len(opened) > 0 && abs(')'-opened[len(opened)-1]) < 3 {
-				opened = opened[:len(opened)-1]
-			} else {
-				return false
-			}
-
-		case ']':
-			if len(opened) > 0 && abs(']'-opened[len(opened)-1]) < 3 {
-				opened = opened[:len(opened)-1]
-			} else {
-				return false
-			}
-
-		case '}':
-			if len(opened) > 0 && abs('}'-opened[len(opened)-1]) < 3 {
-				opened = opened[:len(opened)-1]
-			} else {
-				return false
-			}
-		default:
-			opened = append(opened, s[i])
-		}
-
-	}
-	if len(opened) > 0 {
-		return false
-	}
-	return true
-
-}
-
-func abs(a byte) byte {
-	if a > 0 {
-		return a
-	}
-	return -a
-}
+        return False if stack else True #Если осталась незакрытая скобка
