389f8435a516d2235e2dd4cb8f6473c3b5cb0847
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-05-17 06:21:47 +0300

    Update 933.py

--- tasks/933.py
+++ tasks/933.py
@@ -1,34 +1,17 @@
-package main
+class RecentCounter:
 
-import "fmt"
+    def __init__(self):
+        self.deque = deque()
 
-func main() {
-	obj := Constructor()
-	param_1 := obj.Ping(1)
-	fmt.Print(param_1)
-	param_2 := obj.Ping(100)
-	fmt.Print(param_2)
-	param_3 := obj.Ping(3001)
-	fmt.Print(param_3)
-	param_4 := obj.Ping(3002)
-	fmt.Print(param_4)
+    def ping(self, t: int) -> int:
 
-}
+        self.deque.append(t)
+        while t - 3000 > self.deque[0]:
+            self.deque.popleft()
+        
+        return len(self.deque)
 
-type RecentCounter struct {
-	pings []int
-}
 
-func Constructor() RecentCounter {
-	return RecentCounter{}
-}
-
-func (this *RecentCounter) Ping(t int) int {
-	this.pings = append(this.pings, t)
-	lowest := 0
-	for this.pings[lowest] < t-3000 {
-		lowest++
-	}
-	this.pings = this.pings[lowest:]
-	return len(this.pings)
-}
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
