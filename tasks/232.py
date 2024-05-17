56d9f8e3ba1021adb4c6343119747f241d2cd848
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-05-17 06:44:16 +0300

    Update 232.py

--- tasks/232.py
+++ tasks/232.py
@@ -1,50 +1,31 @@
-package main
+class MyQueue:
 
-import "fmt"
+    def __init__(self):
+        self.stack1 = deque()
+        self.stack2 = deque()
 
-func main() {
-	obj := Constructor()
-	obj.Push(1)
-	obj.Push(2)
-	param_2 := obj.Pop()
-	param_3 := obj.Peek()
-	param_4 := obj.Empty()
-	fmt.Println(param_2)
-	fmt.Println(param_3)
-	fmt.Println(param_4)
-}
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
 
-type MyQueue struct {
-	stack []int
-}
+    def pop(self) -> int:
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        
+        return self.stack2.pop()
 
-/** Initialize your data structure here. */
-func Constructor() MyQueue {
-	return MyQueue{}
-}
+    def peek(self) -> int:
+        peeked = self.pop()
+        self.stack2.append(peeked)
+        return peeked
 
-/** Push element x to the back of queue. */
-func (this *MyQueue) Push(x int) {
-	this.stack = append(this.stack, x)
-}
+    def empty(self) -> bool:
+        return not(self.stack1 or self.stack2)
 
-/** Removes the element from in front of queue and returns that element. */
-func (this *MyQueue) Pop() int {
-	ret := this.stack[0]
-	this.stack = this.stack[1:]
-	return ret
-}
 
-/** Get the front element. */
-func (this *MyQueue) Peek() int {
-	return this.stack[0]
-}
-
-/** Returns whether the queue is empty. */
-func (this *MyQueue) Empty() bool {
-	lenSt := len(this.stack)
-	if lenSt == 0 {
-		return true
-	}
-	return false
-}
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
