5fe92e37442cce3616a9513a82230d8cae7ffe09
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-05-20 04:53:53 +0300

    Update 146.py

--- tasks/146.py
+++ tasks/146.py
@@ -1,89 +1,26 @@
-package main
+# Beats 80% 
 
-import "fmt"
+class LRUCache:
 
-func main() {
-	lru := Constructor(2)
-	lru.Put(2, 1)
-	lru.Put(1, 1)
-	lru.Put(2, 3)
-	lru.Put(4, 1)
-	fmt.Println(lru.head.next)
-	fmt.Println(lru.tail.prev)
-	// lru.Put(2, 2)
-	// // fmt.Println(lru.dLList.head)
-	// lru.Get(1)
-	// lru.Put(3, 3)
-	// fmt.Println(lru.dLList.tail.value)
-	// fmt.Println(lru.Get(2))
+    def __init__(self, capacity: int):
+        self.cache = dict()
+        self.max_capacity = capacity
+    
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        
+        value = self.cache.pop(key)
+        self.put(key, value)
+        return value
 
-}
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            del self.cache[key]
+            self.cache.update({key : value})
+        else:
+            self.cache.update({key : value})
+            if len(self.cache) > self.max_capacity:
+                LRUKey = next(iter(self.cache)) # Finds the first element in cache
+                self.cache.pop(LRUKey)
 
-type LRUCache struct {
-	cacheMap   map[int]*DLNode
-	capacity   int
-	head, tail *DLNode
-}
-
-type DLNode struct {
-	prev  *DLNode
-	next  *DLNode
-	key   int
-	value int
-}
-
-func Constructor(capacity int) LRUCache {
-	lru := LRUCache{
-		cacheMap: make(map[int]*DLNode),
-		capacity: capacity,
-		head:     &DLNode{},
-		tail:     &DLNode{},
-	}
-	lru.head.next, lru.tail.prev = lru.tail, lru.head
-	return lru
-}
-
-func (this *LRUCache) Get(key int) int {
-	n, ok := this.cacheMap[key]
-	if !ok {
-		return -1
-	}
-	this.delete(n)
-	this.pushAfter(this.head, n)
-	return n.value
-}
-
-func (this *LRUCache) Put(key int, value int) {
-	n, ok := this.cacheMap[key]
-	if !ok {
-		if len(this.cacheMap) == this.capacity {
-			endNode := this.tail.prev
-			delete(this.cacheMap, endNode.key)
-			this.delete(endNode)
-			endNode.key, endNode.value = key, value
-			n = endNode
-		} else {
-			n = &DLNode{
-				value: value,
-				key:   key,
-			}
-		}
-		this.cacheMap[key] = n
-	} else {
-		n.value = value
-		this.delete(n)
-	}
-
-	this.pushAfter(this.head, n)
-}
-
-func (this *LRUCache) pushAfter(current, new *DLNode) {
-	next := current.next
-	new.prev, new.next = current, next
-	current.next, next.prev = new, new
-}
-
-func (this *LRUCache) delete(curr *DLNode) {
-	prev, next := curr.prev, curr.next
-	prev.next, next.prev = next, prev
-}
