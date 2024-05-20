9e4d4260b9e1d2fc962ce7bdfd3f800cc2a66f9e
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-05-20 04:59:13 +0300

    Update 268.py

--- tasks/268.py
+++ tasks/268.py
@@ -1,14 +1,4 @@
-package main
-
-func main() {
-
-}
-
-func missingNumber(nums []int) int {
-	n := len(nums)
-	sum := int(float32(1+n) / float32(2) * float32(n))
-	for _, el := range nums {
-		sum -= el
-	}
-	return sum
-}
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return int(n*(n+1)/2 - sum(nums))
