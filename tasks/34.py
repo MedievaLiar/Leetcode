c3e7da769554f9c10f60a367c65928cf293944e3
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-05-20 05:30:36 +0300

    Update 34.py

--- tasks/34.py
+++ tasks/34.py
@@ -1,56 +1,15 @@
-package main
+from bisect import bisect_left
+from bisect import bisect_right
 
-import "fmt"
-
-func main() {
-	nums := []int{1}
-	fmt.Print(searchRange(nums, 0))
-}
-
-func searchRange(nums []int, target int) []int {
-	ind := len(nums) / 2
-	if len(nums) == 0 {
-		return []int{-1, -1}
-	}
-	if target < nums[ind] {
-		for ; ind >= 0 && target != nums[ind]; ind-- {
-			if target > nums[ind] {
-				return []int{-1, -1}
-			}
-		}
-		if ind < 0 {
-			return []int{-1, -1}
-		}
-	}
-	if target > nums[ind] {
-		for ; ind < len(nums) && target != nums[ind]; ind++ {
-			if target < nums[ind] {
-				return []int{-1, -1}
-			}
-		}
-		if ind >= len(nums) {
-			return []int{-1, -1}
-		}
-	}
-	return expandFromMiddle(&nums, target, ind)
-}
-
-func expandFromMiddle(nums *[]int, target, ind int) []int {
-	right := ind
-	left := ind
-	for ; right < len(*nums); right++ {
-		if (*nums)[right] != target {
-			break
-		}
-	}
-
-	for ; left >= 0; left-- {
-		if (*nums)[left] != target {
-			break
-		}
-	}
-	left++
-	right--
-	return []int{left, right}
-
-}
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left_pos = bisect_left(nums, target)
+        right_pos = bisect_right(nums, target) - 1
+        try:
+            l = nums[left_pos]  
+        except IndexError: # In case binsearch didn't find the element and returned the latest index
+            return[-1, -1]
+        else:
+            if l != target: # In case binsearch didn't find the element and returned the index of closes element
+                return [-1, -1]
+            return [left_pos, right_pos]        
