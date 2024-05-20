af820afe890389f5547c1b8c783f79abcf0cfbb1
Otkloneniye <44743326+Otkloneniye@users.noreply.github.com>
2024-05-20 05:33:46 +0300

    Update 28.py

--- tasks/28.py
+++ tasks/28.py
@@ -1,20 +1,3 @@
-package main
-
-func main() {
-
-}
-
-func strStr(haystack string, needle string) int {
-	if len(needle) == 0 {
-		return 0
-	}
-	for ind := range haystack {
-		if len(haystack) < ind+len(needle) {
-			break
-		}
-		if haystack[ind:ind+len(needle)] == needle {
-			return ind
-		}
-	}
-	return -1
-}
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
